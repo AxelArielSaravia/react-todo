09f7539c332ac54854e0e445fb0cf79aa4c50879
axarisar <axelarielsaravia@gmail.com>
2023-07-07 20:39:47 -0300

    fix service worker

--- src/serviceworker.js
+++ src/serviceworker.js
@@ -7,13 +7,35 @@ const urlsToCache = [
 ];
 
 
-async function preCache() {
+async function waitCache() {
     const cache = await caches.open(CACHE_NAME);
     return cache.addAll(urlsToCache);
 }
 
-function install(e) {
-    e.waitUntil(preCache);
+function installListener(e) {
+    e.waitUntil(waitCache());
 }
 
-self.addEventListener("install", install);
+async function fetchCache(req) {
+    const netRes = await fetch(req);
+    const cache = await caches.open(CACHE_NAME);
+    cache.put(req, netRes.clone());
+    return netRes;
+}
+
+async function waitFetch(e) {
+    const res = await caches.match(e.request);
+    const fetchPr = fetchCache(e.request);
+    return (
+        res !== undefined
+        ? res
+        : fetchPr
+    );
+}
+
+function fetchListener(e) {
+    e.waitUntil(waitFetch(e));
+}
+
+self.addEventListener("install", installListener);
+self.addEventListener("fetch", fetchListener);
